a77e43c6caa8859e3ab579b993da39bffe26d28d
forclients2050 <sonu2105072@gmail.com>
2025-05-02 10:02:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,10 @@ const allowedOrigins = [
   'http://localhost:5173',
   'https://greenheadconsultancy.netlify.app',
   'https://www.greenheadconsultancy.netlify.app',
-   'https://greenheadsconsultants.com',
+  'https://greenheadsconsultants.com',
   'https://www.greenheadsconsultants.com',
+  "https://www.greenheadsconsultants.com/"
+  
 ];
 
 app.use(cors({
